c5c6265f5c17d039426c4335d3b2cec073a61151
Arceniktechnologies <arceniktechnologies@gmail.com>
2025-08-26 17:27:00 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useState, lazy, Suspense, useEffect } from "react";
-// Eager load critical pages
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { PageLoader } from "./components/LoadingAnimation";
